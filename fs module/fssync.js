327a921aa71877ad59570f87474bd54eec9b3f30
Tamanna Thakur <tamannardthakur@gmail.com>
2026-09-08 14:32:49 +0530

    directory modules

--- fs module/fssync.js
+++ fs module/fssync.js
@@ -7,7 +7,16 @@ const fs=require(`fs`);
 // const data=fs.readFileSync(`example.txt`,"utf8");
 // console.log("file content is:",data);
 
-fs.writeFileSync(`example1.txt`,`This is the file we use the delete function on`,"utf8");
-fs.unlinkSync(`example1.txt`);
-console.log("File was deleted");
 
+// fs.writeFileSync(`example1.txt`,`This is the file we use the delete function on`,"utf8");
+// fs.unlinkSync(`example1.txt`);
+// console.log("File was deleted");
+// fs.mkdirSync(`New Folder`);
+// console.log("a new folder is created");
+// fs.rmdirSync(`New Folder`);
+// console.log("New folder was deleted");
+if(fs.existsSync(`example.txt`)){
+    console.log("file exists");
+}else{
+    console.log("file does not exist");
+}
