82acfc18d7810964786ba5b866e477da64885b92
Tamanna Thakur <tamannardthakur@gmail.com>
2026-09-09 11:57:33 +0530

    Asyc complete

--- fs module/assync.js
+++ fs module/assync.js
@@ -1,15 +1,45 @@
 const fs=require('fs');
-fs.writeFile("newexample.txt","Welcome to fsd classes","utf-8", (err)=>{
-        if(err){
-            console.log("Error creating file",err);
-        } console.log("file created succesfully");
-    }
-)
-fs.readFile("newexample.txt","utf8",(err,data)=>{
+// fs.writeFile("newexample.txt","Welcome to fsd classes","utf-8", (err)=>{
+//         if(err){
+//             console.log("Error creating file",err);
+//         } console.log("file created succesfully");
+//     }
+// )
+// fs.readFile("newexample.txt","utf8",(err,data)=>{
+//     if(err){
+//          console.log("Error reading file",err);
+//          return;
+//     }
+//     console.log("File content:");
+//     console.log(data);
+// })
+// fs.appendFile("newexample.txt"," Semester 3","utf8",(err,data)=>{
+//     if(err){
+//          console.log("Error updating file",err);
+//          return;
+//     }else{
+//          console.log("File updated successfully");
+//     }
+// })
+// fs.readFile("newexample.txt","utf8",(err,data)=>{
+//     if(err){
+//          console.log("Error reading file",err);
+//          return;
+//     }
+//     console.log("File content:");
+//     console.log(data);
+// })
+// fs.writeFile("delete.txt","Welcome to fsd classes","utf-8", (err)=>{
+//         if(err){
+//             console.log("Error creating file",err);
+//         } console.log("file created succesfully");
+//     }
+// )
+fs.unlink("delete.txt",(err)=>{
     if(err){
-         console.log("Error reading file",err);
+         console.log("Error deleting file",err);
          return;
+    }else{
+        console.log("File deleted successfully");
     }
-    console.log("File content:");
-    console.log(data);
 })
